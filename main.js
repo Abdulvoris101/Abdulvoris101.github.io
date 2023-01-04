83ad9f35834b175e20fcd9e3c53c2dfbbcd6c008
Abdulvoris101 <erkinovabdulvoris101@gmail.com>
2023-01-05 01:26:12 +0500

    CHANGED

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 let tg = window.Telegram.WebApp
 let user = document.getElementById('user')
 
-function onClose () {
-    user.innerHTML = tg.initDataUnsafe.user.first_name
-    console.log(tg.initDataUnsafe.user.first_name);
-    tg.close()
-}
+user.innerHTML = `${tg.initDataUnsafe.user.first_name} - FirstName`
+
