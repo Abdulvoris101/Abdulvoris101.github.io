185ed9f0b92a371136bc1dbc491aa3f91616b8cb
Abdulvoris101 <erkinovabdulvoris101@gmail.com>
2023-01-10 20:30:39 +0500

    Changedd

--- main.js
+++ main.js
@@ -3,9 +3,8 @@ let tg = window.Telegram.WebApp
 
 tg.expand()
 
-const { first_name, last_name, username } = window.Telegram.WebApp.initDataUnsafe.user;
 
 
 let usercard = document.getElementById("usercard")
 
-usercard.innerHTML = first_name
+usercard.innerHTML = tg.backgroundColor
