34d047d5a154a4ef2e25e8e594d59485938f7ac4
Abdulvoris101 <erkinovabdulvoris101@gmail.com>
2023-01-10 20:16:56 +0500

    Changedd

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 let tg = window.Telegram.WebApp
 
-const { first_name, last_name, username } = window.Telegram.WebApp.initDataUnsafe.user;
 
 tg.expand()
+
+const { first_name, last_name, username } = window.Telegram.WebApp.initDataUnsafe.user;
+
 
 let usercard = document.getElementById("usercard")
 
